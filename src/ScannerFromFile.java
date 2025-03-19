39e52b4df2e987410f247406a7ed0b1122730f32
Martu <martavgarcia15@hotmail.com>
2025-03-19 12:04:59 +0100

    1.1 punto 2, ejemplo 1, error

---
+++ src/ScannerFromFile.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+public class ScannerFromFile {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new File("test.in"));
+        // do something ...
+    }
+}
+
