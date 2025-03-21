5e3b4459940a5d0cba681060434040acb1d46125
Martu <martavgarcia15@hotmail.com>
2025-03-22 00:12:47 +0100

    commit error

--- src/Ejercicio_2/MethodCallStackDemo.java
+++ src/Ejercicio_2/MethodCallStackDemo.java
@@ -18,6 +18,7 @@ public class MethodCallStackDemo {
     }
     public static void methodC() {
         System.out.println("Enter methodC()");
+        System.out.println(1 / 0); // divide-by-0 triggers an ArithmeticException
         System.out.println("Exit methodC()");
     }
 }
